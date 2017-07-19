4ccd2b2b1098276eb5a6d5d72094de0ec85be45d
KacperS <kacperstelmach1992@gmail.com>
2017-07-19 19:31:56 +0200

    Finish

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -15,9 +16,22 @@ namespace PartyInvites.Controllers
             ViewBag.Greeting = hour >= 17 ? "Dobry wieczów Panu" : "Dzień dobry Panu";
             return View();
         }
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
