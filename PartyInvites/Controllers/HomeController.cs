28135cf08eaee516859d2927f23fdb7fc7b92456
KacperS <kacperstelmach1992@gmail.com>
2017-07-19 14:29:11 +0200

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour >= 17 ? "Dobry wieczów" : "Dzień dobry";
+            return View();
+        }
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+    }
+}
